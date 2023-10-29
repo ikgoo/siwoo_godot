9a45db82b5c24fd33ad66babfc4e736d6c839e79
ikgoo <lhscj2466@gmail.com>
2023-10-30 07:46:06 +0900

    .

---
+++ Thelighter/Scenes/main_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var animation_player = $CharacterBody2D/AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
